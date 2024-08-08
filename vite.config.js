e25789f7ba2523baeaa52c3f223be3c19a145615
Nihara Padil <niharapadilhr@gmail.com>
2024-08-09 01:23:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-horizontal-scrolling-menu'],
-    },
-  },
+ optimizeDeps: {
+    include: ['react-horizontal-scrolling-menu'],
+},
 });
